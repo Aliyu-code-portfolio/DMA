0649d7b32621446629a199caa3b63940d57cf8af
Aliyu <88856370+AllyCodeX@users.noreply.github.com>
2023-01-06 07:38:09 +0100

    Update to App.js
    Removed my DB files

--- App.js
+++ App.js
@@ -48,14 +48,14 @@ export default function App() {
     }
   }
   const firebaseConfig = {
-    apiKey: "AIzaSyBNN1t82sFc-CWUyyW8Mpsmo4zQnqot63s",
-    authDomain: "dma-5c98f.firebaseapp.com",
-    projectId: "dma-5c98f",
-    storageBucket: "dma-5c98f.appspot.com",
-    messagingSenderId: "601101583020",
-    appId: "1:601101583020:web:08fcb8e80fa3e898fe9509",
-    measurementId: "G-8T01HY2VVN",
-    storageBucket: "gs://dma-5c98f.appspot.com"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
+    storageBucket: ""
 
   };
   if (!firebase.apps.length) {
